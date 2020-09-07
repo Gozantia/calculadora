b611db28ec70e14181a4eecd1dddd05a0e241326
Gozantia <lordzantiago@gmail.com>
2020-09-06 21:33:41 -0500

    subiendo proyecto

--- src/App.js
+++ src/App.js
@@ -1,26 +1,149 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+const nums = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0];
+const ops = [ '/', '*', '-', '+'];
+const ids = {
+  7: 'seven', 
+  8: 'eight', 
+  9: 'nine', 
+  4: 'four', 
+  5: 'five', 
+  6: 'six', 
+  1: 'one', 
+  2: 'two', 
+  3: 'three', 
+  0: 'zero',
+  '/': 'divide', 
+  '*': 'multiply', 
+  '-': 'subtract', 
+  '+': 'add'
 }
-
-export default App;
+export default class App extends Component 
+{
+  state = {
+    lastPressed: undefined,
+    calc: '0',
+    operation: undefined
+  }
+  
+  handleClick = (e) => {
+    const { calc, lastPressed } = this.state;
+    const { innerText } = e.target;
+    
+    switch(innerText) {
+      case 'AC': {
+        this.setState({
+          calc: '0',
+        });
+        break;
+      }
+        
+      case '=': {
+        const evaluated = eval(calc);
+        this.setState({
+          calc: evaluated
+        });
+        break;
+      }
+        
+      case '.': {
+        const splitted = calc.split(/[\+\-\*\/]/);
+        const last = splitted.slice(-1)[0];
+        
+        if(!last.includes('.')) {
+          this.setState({
+            calc: calc+'.'
+          })
+        }
+        
+        break;
+      }
+        
+      default: {
+        let e = undefined;
+        
+        if(ops.includes(innerText)) {
+          if(ops.includes(lastPressed) && innerText !== '-') {
+            
+            const lastNumberIdx = calc.split('').reverse()
+                .findIndex(char => char !== ' ' && nums.includes(+char)); 
+            e = calc.slice(0, calc.length - lastNumberIdx) + ` ${innerText} `;
+          } else {
+            e = `${calc} ${innerText} `;
+          }
+        } else {
+          e = (calc === '0') ? innerText : (calc + innerText);
+        }
+        
+        this.setState({
+          calc: e
+        });
+      }
+    }
+    
+    this.setState({
+      lastPressed: innerText
+    })
+    
+  }
+  
+  render() {
+    const { calc } = this.state;
+    
+    return (
+      <div className="calculadora">        
+        <div id="display" className="display">
+          {calc}
+        </div>
+        
+        <div className="nums-container">
+          <button 
+            className="big-h light-grey ac" 
+            onClick={this.handleClick} 
+            id="clear"
+            >
+            C
+          </button>
+          
+          {nums.map(num => (
+            <button 
+              className={`dark-grey ${num === 0 && 'big-h'}`} 
+              key={num} 
+              onClick={this.handleClick}
+              id={ids[num]}
+             >
+              {num}
+            </button>
+          ))}
+          
+          <button 
+            className="light-grey" 
+            onClick={this.handleClick} 
+            id="decimal"
+           >
+            .
+          </button>
+        </div>
+        <div className="ops-container">
+          {ops.map(op => (
+            <button 
+              className="verde" 
+              key={op} 
+              onClick={this.handleClick}
+              id={ids[op]}
+             >
+              {op}
+            </button>
+          ))}
+          
+          <button 
+            className="verde" 
+            onClick={this.handleClick} 
+            id="equals"
+           >
+            =
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
