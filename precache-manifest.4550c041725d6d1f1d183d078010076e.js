b6534eb66ea3a6ec39da645c5e67921af1d79077
Gozantia <lordzantiago@gmail.com>
2020-09-06 21:39:03 -0500

    Updates

---
+++ precache-manifest.4550c041725d6d1f1d183d078010076e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09eeee3e2c036d03f94769c58fa79c01",
+    "url": "/calculadora/index.html"
+  },
+  {
+    "revision": "aeaf0207b9fc894c3b83",
+    "url": "/calculadora/static/css/main.1105c3cb.chunk.css"
+  },
+  {
+    "revision": "2835fbb0a47d233dc860",
+    "url": "/calculadora/static/js/2.bf858ff0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculadora/static/js/2.bf858ff0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aeaf0207b9fc894c3b83",
+    "url": "/calculadora/static/js/main.e0adc95c.chunk.js"
+  },
+  {
+    "revision": "10a74285f2769b94f6ac",
+    "url": "/calculadora/static/js/runtime-main.91648a95.js"
+  }
+]);
